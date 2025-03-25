369bcd7192b21bb8a35ee445a4d3e1a2d17f7505
Allovallo <grossco@meta.ua>
2025-03-25 16:10:02 +0200

    files updated

---
+++ app.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const DB_HOST =
+  "mongodb+srv://grossco:poiP36Z7WnTvbTLJ@cluster0.2qtkt.mongodb.net/books_reader?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database connected!"))
+  .catch((error) => console.log(error.message));
